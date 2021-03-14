a36e0b6c8183daf988330dc79929f2817aae7794
João Dantas <joaostadantas@gmail.com>
2021-03-13 21:06:54 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,48 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-  }
+
+      if (!newTaskTitle) return;
+
+      setTasks([...tasks, {
+        id: Math.random()*10000,
+        title: newTaskTitle,
+        isComplete: false,
+      }])
+
+      setNewTaskTitle('');
+    }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const [taskToChange] = tasks.filter((task)=> task.id === id)
+    const taskIndex = tasks.indexOf(taskToChange)
+
+    const newTasks = tasks.map((task)=>{
+      if (taskIndex == tasks.indexOf(task)) {
+        return({
+          id: task.id,
+          title: task.title,
+          isComplete: !task.isComplete,
+        })
+      }
+
+      return task
+    })
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const [taskToChange] = tasks.filter((task)=> task.id === id)
+    const taskIndex = tasks.indexOf(taskToChange)
+    const newTasks = tasks.map((e)=>e)
+    
+    newTasks.splice(taskIndex, 1)
+
+    setTasks(newTasks)  
   }
 
   return (
